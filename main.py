24d08dc2b03db9ffe81b7b8377c5165ea00907ec
Brian <ibpak.why@gmail.com>
2026-03-09 16:13:12 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+customers_list = [('Dave',47,'Member'),('Gwendolyn',32,'Nonmember'),('Anne',23,'Member')]
+customers_dict = {'Dave':(47,'Member'),'Gwendolyn': (32,'Nonmember'),'Anne':(23,'Member')}
+tupel = ("tupel 1", "tupel 2")
+print (tupel[0])
